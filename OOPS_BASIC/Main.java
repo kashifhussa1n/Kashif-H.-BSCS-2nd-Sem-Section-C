fa76e58f6a464f03014d664b100958afa6eae02c
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 01:19:25 +0500

    oops

--- OOPS_BASIC/Main.java
+++ OOPS_BASIC/Main.java
@@ -5,5 +5,11 @@ public class Main {
         // and can perform actions (methods)
         // It is a reference data types
 
+        // Object Creation
+        // class | name = create new object | Constructor(reference)
+        // Scanner scanner = new Scanner(System.in);
+
+        Car car = new Car();
+        System.out.println(car.model);
     }
 }
