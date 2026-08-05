5920138692543dac1f98d7ac287a5a617246b623
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 01:31:36 +0500

    Create Main.java

---
+++ CONSTRUCTORS/Main.java
@@ -0,0 +1,10 @@
+package CONSTRUCTORS;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // constructor= A special method to intialize objects
+        // You can pass arguments to a constructor and setup initial values
+
+    }
+}
