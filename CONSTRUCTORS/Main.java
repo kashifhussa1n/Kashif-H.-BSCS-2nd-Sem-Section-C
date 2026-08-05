0cbcd4d15fb283ef5016a3ee56a302f0d2273ed3
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 01:59:38 +0500

    s

--- CONSTRUCTORS/Main.java
+++ CONSTRUCTORS/Main.java
@@ -6,5 +6,21 @@ public class Main {
         // constructor= A special method to intialize objects
         // You can pass arguments to a constructor and setup initial values
 
+        Student student1 = new Student();
+        Student student2 = new Student();
+        Student student3 = new Student();
+
+        System.out.println(student1.name);
+        System.out.println(student2.name); // all would be named Spongebob
+        System.out.println(student3.name);
+
+        // what if we wanna give each one unique name(Constructor)
+        Student student1 = new Student("Spongebob", 35, 3.9);
+        Student student2 = new Student("Patrick", 32, 0.9);
+        Student student3 = new Student("Squidward", 48, 2.0);
+
+        System.out.println(student1.name);
+        System.out.println(student2.name); // all would be named Spongebob
+        System.out.println(student3.name);
     }
 }
--- CONSTRUCTORS/Student.java
+++ CONSTRUCTORS/Student.java
@@ -4,5 +4,12 @@ public class Student {
     String name = "Spongebob";
     int age;
     double gpa;
-    boolean isEnrolled;
+
+    Student(String name, int age, double gpa) { // Constructor
+        this.name = name;
+        this.age = age; // this(keyword) = the object we're currently working with/constructing
+        this.gpa = gpa;
+    }
+
+    // note: Constructor will have same name as main class e.g. Student Student
 }
