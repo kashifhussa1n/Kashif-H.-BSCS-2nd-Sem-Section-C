983cad44eba032f2ae71c12d4bc217319a4ee98f
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 02:46:32 +0500

    s

---
+++ POINT_TRIANGLE/Point.java
@@ -0,0 +1,12 @@
+package POINT_TRIANGLE;
+
+public class Point {
+
+    private double x;
+    private double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
