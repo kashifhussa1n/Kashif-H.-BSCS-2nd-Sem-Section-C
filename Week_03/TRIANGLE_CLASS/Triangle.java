ffbf223a23ae2f5d0d194e2702a704b3a9d823e4
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 12:10:50 +0500

    Update Triangle.java

--- Week_03/TRIANGLE_CLASS/Triangle.java
+++ Week_03/TRIANGLE_CLASS/Triangle.java
@@ -11,4 +11,25 @@ public class Triangle {
         this.sideC = 1.0;
     }
 
+    // one parameter constructor
+    Triangle(double x) {
+        this.sideA = x;
+        this.sideB = x;
+        this.sideC = x;
+    }
+
+    // two parameter constructor
+    Triangle(double x, double y) {
+        this.sideA = x;
+        this.sideB = x;
+        this.sideC = y;
+    }
+
+    // three parameter constructor
+    Triangle(double x, double y, double z) {
+        this.sideA = x;
+        this.sideB = y;
+        this.sideC = z;
+    }
+
 }
