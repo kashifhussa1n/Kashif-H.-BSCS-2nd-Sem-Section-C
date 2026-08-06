f7eed37a501e6751352ca42e3ef60db1cab790b0
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 11:52:02 +0500

    s

--- Week_03/TRIANGLE_CLASS/Main.java
+++ Week_03/TRIANGLE_CLASS/Main.java
@@ -2,6 +2,6 @@ package TRIANGLE_CLASS;
 
 public class Main {
     public static void main(String[] args) {
-       
+
     }
 }
--- Week_03/TRIANGLE_CLASS/Triangle.java
+++ Week_03/TRIANGLE_CLASS/Triangle.java
@@ -2,4 +2,13 @@ package TRIANGLE_CLASS;
 
 public class Triangle {
 
+    private double sideA, sideB, sideC;
+
+    // Constructor
+    Triangle() {
+        this.sideA = 1.0;
+        this.sideB = 1.0;
+        this.sideC = 1.0;
+    }
+
 }
