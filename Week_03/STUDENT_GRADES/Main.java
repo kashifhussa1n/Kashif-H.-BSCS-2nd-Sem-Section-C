971d1a321f20803b1d1285614b9b67deedb951b1
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 11:16:53 +0500

    S

--- Week_03/STUDENT_GRADES/Main.java
+++ Week_03/STUDENT_GRADES/Main.java
@@ -2,17 +2,6 @@ package STUDENT_GRADES;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Joe");
-        Student student2 = new Student("Mary");
-
-        student1.inputGrades();
-        student2.inputGrades();
-
-        System.out.println("The average for " + student1.getName() + " is " + student1.getAverage() + ".");
-        System.out.println("The average for " + student2.getName() + " is " + student2.getAverage() + ".");
-
-        // Printing the objects directly -- this calls toString() automatically
-        System.out.println("Student 1: " + student1);
-        System.out.println("Student 2: " + student2);
+        
     }
 }
