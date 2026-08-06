e4c2b0a030e56968a8ee8bbe39ea6243c5d99d20
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-06 21:20:27 +0500

    s

--- Week_03/STUDENT_GRADES/Main.java
+++ Week_03/STUDENT_GRADES/Main.java
@@ -2,6 +2,22 @@ package STUDENT_GRADES;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Student student1 = new Student("Ali");
+        Student student2 = new Student("Kassam");
+
+        student1.inputGrades();
+        student2.inputGrades();
+
+        student1.printName();
+        student2.printName();
+
+        // manually print getName method(return type)
+        System.out.println("getName() - Student Name: " + student1.getName());
+        System.out.println("getName() - Student Name: " + student2.getName());
+
+        System.out.println(student1);
+        System.out.println(student2);
+
     }
 }
--- Week_03/STUDENT_GRADES/Student.java
+++ Week_03/STUDENT_GRADES/Student.java
@@ -1,7 +1,58 @@
 package STUDENT_GRADES;
 
-
+import java.util.Scanner;
 
 public class Student {
-    
+    private String studentName;
+    private double scoreTest1, scoreTest2;
+
+    // constructor
+    Student(String studentName) {
+        this.studentName = studentName;
+    }
+
+    // method
+    void printName() {
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Average Test Score: " + getAverage());
+    }
+
+    // input method
+    void inputGrades() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter First Test Score of " + studentName + ": ");
+        scoreTest1 = scanner.nextDouble();
+        while (scoreTest1 < 0 || scoreTest1 > 100) {
+            System.out.println("ERROR! Number should be between 0 and 100. Retry: ");
+            scoreTest1 = scanner.nextDouble();
+        }
+
+        System.out.print("Enter Second Test Score of " + studentName + ": ");
+        scoreTest2 = scanner.nextDouble();
+        while (scoreTest2 < 0 || scoreTest2 > 100) {
+            System.out.print("ERROR! Number should be between 0 and 100. Retry: ");
+            scoreTest2 = scanner.nextDouble();
+        }
+
+    }
+
+    // average method
+    double getAverage() {
+        return (scoreTest1 + scoreTest2) / 2;
+    }
+
+    // get name method
+    String getName() {
+        return studentName;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "[With toString] Student Name: " + studentName + " Test 01 Score: " + scoreTest1 + " Test 02 Score: "
+                + scoreTest2 + "\n[Without toString] STUDENT_GRADES.Student@4fca772d\r\n" + //
+                "STUDENT_GRADES.Student@9807454";
+    }
+
 }
