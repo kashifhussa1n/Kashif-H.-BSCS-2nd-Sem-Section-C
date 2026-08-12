0410f9b112d1ef11df1978ae16fd9fd86929bd71
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-13 02:02:50 +0500

    s

--- Week_04/LINE_INTERSECTION/Line.java
+++ Week_04/LINE_INTERSECTION/Line.java
@@ -1,5 +1,43 @@
 package Week_04.LINE_INTERSECTION;
 
-public class Line {
+class Line {
+
+    // since; y = mx + b
+    // then; x = (y - b) / m
+    /*
+     * Line 1: y = m1x + b1
+     * Line 2: y = m2x + b2
+     * 
+     * Intersection:
+     * m1x + b1 = m2x + b2
+     * 
+     * x = (b2 - b1) / (m1 - m2)
+     * 
+     * y = m1x + b1
+     * 
+     * Result = (x, y)
+     */
+
+    private double m, b;
+
+    Line(double m, double b) { // parametrized constructor
+        this.m = m;
+        this.b = b;
+    }
+
+    // Intersection METHOD
+    void Intersection(Line other) {
+
+        if (this.m == other.b) {
+            System.out.println("These lines are parallel");
+
+        }
+
+        double x = (other.b - this.b) / (this.m - other.m);
+        double y = this.m * x + this.b;
+
+        System.out.println("Intersection: (" + x + ", " + y + ")");
+
+    }
 
 }
--- Week_04/LINE_INTERSECTION/Main.java
+++ Week_04/LINE_INTERSECTION/Main.java
@@ -1,1 +1,16 @@
 package Week_04.LINE_INTERSECTION;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Line 1: y = 2x - 1
+        Line line1 = new Line(2, -1);
+
+        // Line 2: y = -2x + 7
+        Line line2 = new Line(-2, 7);
+
+        // Find Intersection
+        line1.Intersection(line2);
+    }
+}
