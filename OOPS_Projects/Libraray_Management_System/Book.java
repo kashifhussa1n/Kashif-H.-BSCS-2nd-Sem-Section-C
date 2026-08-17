1ca1e5495ab625fb2637afeee9fcfca37e2caaa5
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-17 17:11:38 +0500

    Update Book.java

--- OOPS_Projects/Libraray_Management_System/Book.java
+++ OOPS_Projects/Libraray_Management_System/Book.java
@@ -4,11 +4,43 @@ public class Book {
     private String title, author, isbn;
     private boolean isBorrowed;
 
+    // getter
     String getTitle() {
         return this.title;
     }
 
+    // setter
     void setTitle(String title) {
         this.title = title;
+    }
+
+    // getter
+    public String getAuthor() {
+        return this.author;
+    }
+
+    // setter
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // getter
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    // setter
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    // getter
+    public boolean isBorrowed() {
+        return this.isBorrowed;
+    }
+
+    // setter
+    public void setIsBorrowed(boolean isBorrowed) {
+        this.isBorrowed = true;
     }
 }
