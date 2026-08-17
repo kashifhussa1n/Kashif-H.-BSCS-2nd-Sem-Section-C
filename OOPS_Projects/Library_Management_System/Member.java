295073047b5c7b63182765c3bc2011d3e012e38b
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-17 18:02:34 +0500

    s

--- OOPS_Projects/Library_Management_System/Member.java
+++ OOPS_Projects/Library_Management_System/Member.java
@@ -1,14 +1,9 @@
 package OOPS_Projects.Library_Management_System;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Member {
 
     private String name;
     private int id;
-
-    private List<Book> borrowedBooks = new ArrayList<>();
 
     public Member(String name, int id) {
         this.name = name;
@@ -24,17 +19,4 @@ public class Member {
         return this.id;
     }
 
-    public List<Book> getBorrowedBooks() {
-        return borrowedBooks;
-    }
-
-    // METHOD to cap borrowBook at 3
-    public boolean borrowBooks(Book book) {
-        if (borrowedBooks.size() < 3) {
-            borrowedBooks.add(book);
-            return true;
-        }
-        return false;
-
-    }
 }
