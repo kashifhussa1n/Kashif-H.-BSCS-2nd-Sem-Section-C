9457772bf600485e3f918204fe7e7a75e9fb7b18
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-17 17:05:01 +0500

    s

--- OOPS_Projects/Libraray_Management_System/Book.java
+++ OOPS_Projects/Libraray_Management_System/Book.java
@@ -1,5 +1,14 @@
 package OOPS_Projects.Libraray_Management_System;
 
 public class Book {
-    
+    private String title, author, isbn;
+    private boolean isBorrowed;
+
+    String getTitle() {
+        return this.title;
+    }
+
+    void setTitle(String title) {
+        this.title = title;
+    }
 }
--- OOPS_Projects/Libraray_Management_System/Member.java
+++ OOPS_Projects/Libraray_Management_System/Member.java
@@ -2,15 +2,6 @@ package OOPS_Projects.Libraray_Management_System;
 
 public class Member {
 
-    private String title, author, isbn;
-    private boolean isBorrowed;
-
-    String getTitle() {
-        return this.title;
-    }
-
-    void setTitle(String title) {
-        this.title = title;
-    }
+    
 
 }
