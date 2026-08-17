afa24ada998615380c07acda2539b1352822bf15
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-17 18:48:26 +0500

    Update Main.java

--- OOPS_Projects/Library_Management_System/Main.java
+++ OOPS_Projects/Library_Management_System/Main.java
@@ -3,5 +3,11 @@ package OOPS_Projects.Library_Management_System;
 public class Main {
     public static void main(String[] args) {
 
+
+        // books
+        Book book1 = new Book("Harry Potter", "J.K. Rowling", 101);
+        Book book2 = new Book("Atomic Habits", "James Clear", 102);
+        Book book3 = new Book("The Hobbit", "J.R.R. Tolkien", 103);
+        Book book4 = new Book("1984", "George Orwell", 104);
     }
 }
