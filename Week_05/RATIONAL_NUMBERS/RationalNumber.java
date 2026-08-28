12ada44be90498b0e9e1c5c6ba6fb217538586de
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-08-28 15:54:41 +0500

    Update RationalNumber.java

--- Week_05/RATIONAL_NUMBERS/RationalNumber.java
+++ Week_05/RATIONAL_NUMBERS/RationalNumber.java
@@ -4,11 +4,12 @@ public class RationalNumber {
     private double numerator, denominator;
 
     public RationalNumber() { // Null Constructor
+        numerator = 1;
+        denominator = 1;
+    }
 
-
-
-
-
-        
+    public RationalNumber(int numerator, int denominator) { // parametrized
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 }
