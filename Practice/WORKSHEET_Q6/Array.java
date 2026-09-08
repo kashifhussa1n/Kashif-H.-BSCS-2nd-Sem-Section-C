48e491f2c5f4e12bb40d4e4285da00042000b893
Kashif Hussain <147105335+kashifhussa1n@users.noreply.github.com>
2026-09-08 22:16:33 +0500

    s

--- Practice/WORKSHEET_Q6/Array.java
+++ Practice/WORKSHEET_Q6/Array.java
@@ -3,26 +3,34 @@ package WORKSHEET_Q6;
 import java.util.Arrays;
 
 public class Array implements Cloneable {
-    private int[] Arr;
+    private int[] anArray;
     private int value;
 
-    public Array() {
-        Arr = new int[]{1, 2, 3, 4, 5};
-        value = 0;
+    public Array() { // null constructor
+        this.anArray = new int[] { 1, 2, 3, 4, 5, 6 };
+        this.value = 0;
     }
 
     public void increment() {
-        for (int i = 0; i < Arr.length; i++) { Arr[i]++; }
-        value++;
+        this.anArray = new int[6];
+        for (int i = 0; i < this.anArray.length; i++) {
+            this.anArray[i]++;
+        }
+        this.value++;
     }
 
     @Override
     public String toString() {
-        return "Array=" + Arrays.toString(Arr) + ", value=" + value;
+        String print = ""; // empty string for concatenation
+        for (int i = 0; i < this.anArray.length; i++) {
+            print = print + this.anArray[i];
+        }
+
+        return "Array = [" + print + "] - Value = " + this.value;
     }
 
     @Override
     public Array clone() throws CloneNotSupportedException {
-        return (Array) super.clone();   // shallow copy
+        return (Array) super.clone(); // shallow copy
     }
 }
